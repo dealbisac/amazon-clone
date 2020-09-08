1739c323c6f8e853f5dc72f19e1a7789c2352901
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2020-09-09 01:30:17 +0545

    header-top

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
+    //BEM
     <div className="app">
-      <p>Let's Build Amazon Clone</p>
+      {/* Header */}
+      <Header />
+      {/* Home */}
     </div>
   );
 }
