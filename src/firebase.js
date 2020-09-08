fe2a78f65c8eb5922af02b078f9a2198f0046f09
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2020-09-09 00:40:06 +0545

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDNFMF7mLysNZYPqqBb_r3bbv9q0nOsoIw",
+    authDomain: "clone-5153f.firebaseapp.com",
+    databaseURL: "https://clone-5153f.firebaseio.com",
+    projectId: "clone-5153f",
+    storageBucket: "clone-5153f.appspot.com",
+    messagingSenderId: "145116727028",
+    appId: "1:145116727028:web:84c7b96b14e88cf4e5c0a1",
+    measurementId: "G-HM2LZ8GBLY"
+};
